8d6e6b4f35fd075587195cd39a05963947fec92a
Priyadarshi Raj <praj-foss@users.noreply.github.com>
2025-03-19 00:57:32 +0530

    feat: add delta time in game loop

--- mortar/src/main/java/in/praj/mortar/MainApp.java
+++ mortar/src/main/java/in/praj/mortar/MainApp.java
@@ -1,32 +1,66 @@
 package in.praj.mortar;
 
-import javax.microedition.lcdui.Canvas;
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Graphics;
+import javax.microedition.lcdui.game.GameCanvas;
 import javax.microedition.midlet.MIDlet;
 
 public class MainApp extends MIDlet {
     private Display display;
-    private Canvas canvas;
+    private MyGameCanvas canvas;
 
-    static class MyGameCanvas extends Canvas {
-        public void paint(Graphics g) {
-            // clear
-            g.setColor(0);
-            g.fillRect(0, 0, getWidth(), getHeight());
+    static class MyGameCanvas extends GameCanvas implements Runnable {
+        private double z = 0;
+        private double zSpeed = 0.25;
+        private long prevTime;
+        private Runnable onExit;
 
-            drawNoise(g);
+        protected MyGameCanvas(boolean b) {
+            super(b);
+            prevTime = System.currentTimeMillis();
+        }
+
+        public void setOnExit(Runnable onExit) {
+            this.onExit = onExit;
+        }
+
+        public void run() {
+            while (true) {
+                long now = System.currentTimeMillis();
+                double dt = (now - prevTime)/1000d;
+                prevTime = now;
+
+                int ks = getKeyStates();
+                if ((ks & DOWN_PRESSED) != 0) {
+                    break;
+                }
+                if ((ks & RIGHT_PRESSED) != 0) {
+                    z += zSpeed * dt;
+                }
+                if ((ks & LEFT_PRESSED) != 0) {
+                    z -= zSpeed * dt;
+                }
+
+                drawNoise(getGraphics());
+                flushGraphics();
+
+                System.out.println("OK " + dt);
+            }
+            if (onExit != null) onExit.run();
         }
 
         public void drawNoise(Graphics g) {
-            double F = 1d/30;
-            double N = 0.6;
             g.setColor(0xff0000);
+            g.fillRect(0, 0, getWidth(), getHeight());
+
+            double F = 1d/10;
+            double N = 0;
+            g.setColor(0);
             for (int x = 0; x < getWidth(); x++) {
                 for (int y = 0; y < getHeight(); y++) {
-                    double noise = OpenSimplex2S.noise2_ImproveX(0, x * F, y * F);
+                    double noise = OpenSimplex2S.noise3_ImproveXY(0, x * F, y * F, z);
                     if (noise > N) {
-                        g.drawLine(x, y, x, y);
+                        g.drawLine(x, y, x+1, y+1);
                     }
                 }
             }
@@ -36,10 +70,17 @@ public class MainApp extends MIDlet {
     protected void startApp() {
         try {
             display = Display.getDisplay(this);
-            canvas = new MyGameCanvas();
+            canvas = new MyGameCanvas(false);
+            canvas.setOnExit(new Runnable() {
+                public void run() {
+                    destroyApp(true);
+                    notifyDestroyed();
+                }
+            });
 
             display.setCurrent(canvas);
             canvas.setFullScreenMode(true);
+            new Thread(canvas).start();
         } catch (Exception e) {
             e.printStackTrace();
         }
